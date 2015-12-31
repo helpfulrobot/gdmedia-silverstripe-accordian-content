096ebc6e5d4382fc2760e3345dfb450694d50d6f
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:43:18 +1300

    Converted to PSR-2

--- code/extensions/AccordianControllerExtension.php
+++ code/extensions/AccordianControllerExtension.php
@@ -1,12 +1,13 @@
 <?php
 
-class AccordianControllerExtension extends Extension {
+class AccordianControllerExtension extends Extension
+{
 
-    public function onAfterInit() {
+    public function onAfterInit()
+    {
         Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
         Requirements::javascript(FRAMEWORK_DIR . '/thirdparty/jquery-ui/jquery-ui.js');
         Requirements::javascript("silverstripe-accordian-content/assets/javascript/main.js");
         Requirements::css("silverstripe-accordian-content/assets/css/styles.css");
     }
-
 }
